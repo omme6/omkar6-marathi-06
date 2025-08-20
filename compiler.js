8b5fbb3dfb4b7f1502cf23be9ab5a7d34571e8b1
omme6 <wayalomkar08@gmail.com>
2025-08-20 18:15:04 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -7,3 +7,12 @@ function marathiToJs(code) {
     .replace(/नाहीतर/g, "else")        // नाहीतर = else
     .replace(/पर्यंत/g, "while");      // पर्यंत = while
 }
+// Marathi → JavaScript translator
+function marathiToJS(code) {
+    // "लिहा(...)" => console.log(...)
+    code = code.replace(/लिहा\((.*)\)/g, "console.log($1)");
+    // "सुरू_करा" -> function सुरू_करा()
+    code = code.replace(/सुरू_करा/g, "function सुरू_करा()");
+    code = code.replace(/समाप्त/g, "}");
+    return code;
+}
