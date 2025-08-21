10926cc8309814517154adad8d3bc40437960bf2
omme6 <wayalomkar08@gmail.com>
2025-08-21 19:06:25 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -17,3 +17,25 @@ function marathiToJS(code) {
     code = code.replace(/समाप्त/g, "}");
     return code;
 }
+function runCode() {
+    let code = document.getElementById("code").value;
+    let jsCode = marathiToJS(code);
+
+    let outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = ""; // clear old output
+
+    try {
+        // console.log capture करतो
+        let oldLog = console.log;
+        console.log = function(msg) {
+            outputDiv.innerHTML += msg + "<br>";
+            oldLog.apply(console, arguments);
+        }
+
+        eval(jsCode); // JS code run
+
+        console.log = oldLog; // restore
+    } catch (err) {
+        outputDiv.innerHTML = "त्रुटी: " + err.message;
+    }
+}
