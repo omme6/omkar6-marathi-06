a054617d2f5e5a184325aabf3d9684de79289b4d
omme6 <wayalomkar08@gmail.com>
2025-08-21 19:52:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-function runCode() {
-    let code = document.getElementById("editor").value;
-    let result = marathiCompiler(code);
-    document.getElementById("output").textContent = result;
-}
 unction runCode() {
-    let code = document.getElementById("code").value;
-
-    // compiler.js मधून मराठी -> JavaScript मध्ये रूपांतर
-    let jsCode = compileToJS(code);
-
-    try {
-        let result = eval(jsCode);
-        document.getElementById("output").innerText = result;
-    } catch (err) {
-        document.getElementById("output").innerText = "Error: " + err;
-    }
+  let code = document.getElementById("codeInput").value;
+  let result = marathiCompiler(code);
+  document.getElementById("output").innerText = result;
 }
